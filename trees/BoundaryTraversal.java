0a05cc5acb6a3c9cf311579f9112a1fe504f476c
shivam <shivamvish349@gmail.com>
2024-09-01 20:51:15 +0530

    added more problems

---
+++ trees/BoundaryTraversal.java
@@ -0,0 +1,77 @@
+package trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoundaryTraversal 
+{
+    private boolean checkLeafNode(TreeNode node)
+    {
+        if(node.left==null && node.right==null)
+            return true;
+        return false;
+    }
+
+    private void addLeftBoundaryNode(TreeNode node, List<Integer>res)
+    {
+        var leftNode=node.left;
+        while(leftNode!=null)
+        {
+            if(!checkLeafNode(leftNode))
+            {
+                res.add(leftNode.val);
+            }
+            if(leftNode.left!=null)
+                addLeftBoundaryNode(leftNode.left, res);
+            else
+                leftNode=leftNode.right;
+        }
+    }
+
+    private void addRightBoundaryNode(TreeNode node, List<Integer>res)
+    {
+        var rightNode=node.right;
+        List<Integer>r=new ArrayList<>();
+        while(rightNode!=null)
+        {
+            if(!checkLeafNode(rightNode))
+            {
+                r.add(rightNode.val);
+            }
+            if(rightNode.right!=null)
+                addLeftBoundaryNode(rightNode.right, res);
+            else
+                rightNode=rightNode.left;
+        }
+        for(var i=r.size()-1;i>=0;i--)
+        {
+            res.add(r.get(i));
+        }
+    }
+
+    private void addLeafNode(TreeNode node, List<Integer>res)
+    {
+        if(checkLeafNode(node))
+        {
+            res.add(node.val);
+            return;
+        }
+        if(node.left!=null)
+            addLeafNode(node.left, res);
+        if(node.right!=null)
+            addLeafNode(node.right, res);
+    }
+
+    public List<Integer>boundaryTraversal(TreeNode root)
+    {
+        List<Integer>res=new ArrayList<>();
+        addLeftBoundaryNode(root,res);
+        addLeafNode(root, res);
+        addRightBoundaryNode(root, res);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello from somewhere");
+    }
+}
---
+++ trees/ZigZagTraversal.java
@@ -0,0 +1,54 @@
+package trees;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class ZigZagTraversal 
+{
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) 
+    {
+        if(root==null)
+        {
+            return new ArrayList<>();
+        }
+        Queue<TreeNode>q=new ArrayDeque<>();
+        List<List<Integer>>res=new ArrayList<>();
+        q.add(root);
+        int l=0;
+        while(!q.isEmpty())
+        {
+            int size=q.size();
+            List<Integer>temp=new ArrayList<>();
+            for(var i=0;i<size;i++)
+            {
+                if(l%2==0)
+                {
+                    var node=q.poll();
+                    temp.add(node.val);
+                    if(node.left!=null)
+                        q.add(node.left);
+                    if(node.right!=null)
+                        q.add(node.right);
+                }
+                else
+                {
+                    var node=q.poll();
+                    temp.add(node.val);
+                    if(node.right!=null)
+                        q.add(node.right);
+                    if(node.left!=null)
+                        q.add(node.left);
+                }
+            }
+            res.add(temp);
+            l++;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
