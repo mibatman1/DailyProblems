f1de5b7be23a3fcc0a0fe3a819537a11b343e295
shivam <shivamvish349@gmail.com>
2024-09-15 15:52:48 +0530

    added more problems

---
+++ slidingwindow/FirstNveInEveryWIndow.java
@@ -0,0 +1,56 @@
+package slidingwindow;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class FirstNveInEveryWIndow 
+{
+    public long[] printFirstNegativeInteger(long A[], int N, int K)
+    {
+        List<Long>temp=new ArrayList<>();
+        int i=0,j=0;
+        Queue<Long>q=new ArrayDeque<>();
+        while(j<A.length)
+        {
+            if(A[j]<0)
+            {
+                System.out.println(A[j]);
+                q.add(A[j]);
+                j++;
+                System.out.println(q);
+            }
+            else if(j-i+1==K)
+            {
+                if(q.size()==0)
+                {
+                    temp.add(0L);
+                }
+                else
+                {
+                    temp.add(q.poll());
+                }
+                i++;
+                j++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+        long res[]=new long[temp.size()];
+        for(int o=0;o<temp.size();o++)
+        {
+            res[o]=temp.get(o);
+            System.out.println(temp);
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        long p[]={12, -1, -7, 8, -15, 30, 16, 28};
+        FirstNveInEveryWIndow firstNveInEveryWIndow=new FirstNveInEveryWIndow();
+        long res[]=firstNveInEveryWIndow.printFirstNegativeInteger(p, 8, 3);
+        //System.out.println(res);
+    }
+}
---
+++ slidingwindow/MaxSubArraySum.java
@@ -0,0 +1,37 @@
+package slidingwindow;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MaxSubArraySum 
+{
+    long maximumSumSubarray(int K, ArrayList<Integer> Arr,int N)
+    {
+        int sum=0, max=Integer.MIN_VALUE, i=0, j=0;
+        while(j<Arr.size())
+        {
+            sum+=Arr.get(j);
+            if(j-i+1==N)
+            {
+                max=Math.max(max, sum);
+                sum-=Arr.get(i);
+                j++;
+                i++;
+            }
+            else
+            {
+                j++;
+            } 
+        }
+        return max;        
+    }
+
+    public static void main(String[] args) {
+        MaxSubArraySum maxSubArraySum=new MaxSubArraySum();
+        List<Integer>s=Arrays.asList(100, 200, 300, 400);
+        ArrayList<Integer>ar=new ArrayList<>();
+        ar.addAll(s);
+        System.out.println(maxSubArraySum.maximumSumSubarray(4, ar, 2));
+    }
+}
