7bcfe659b620e8eaf7e619bf9d38280bd918b3dc
shivam <shivamvish349@gmail.com>
2024-12-27 17:05:37 +0530

    added more problems

--- recursion/ClimbingStairs.java
+++ recursion/ClimbingStairs.java
@@ -5,6 +5,15 @@ public class ClimbingStairs
 {
     public int climbStairs(int n) 
     {
-           
+        if(n==1)
+            return 1;
+        else if(n==2)
+            return 2;
+        return climbStairs(n-1)+climbStairs(n-2);
+    }
+
+    public static void main(String[] args) {
+        ClimbingStairs climbingStairs=new ClimbingStairs();
+        System.out.println(climbingStairs.climbStairs(10));
     }
 }
