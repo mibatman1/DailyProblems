9e669e8c1b4398e3e46e4609a43092ade36058c9
shivam <shivamvish349@gmail.com>
2024-12-29 21:25:29 +0530

    added more problem

---
+++ recursion/FairDistributionOfCookies.java
@@ -0,0 +1,9 @@
+package recursion;
+
+public class FairDistributionOfCookies 
+{
+    public int distributeCookies(int[] cookies, int k) 
+    {
+            
+    }
+}
--- recursion/LetterCasePermutation.java
+++ recursion/LetterCasePermutation.java
@@ -1,10 +1,38 @@
 package recursion;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LetterCasePermutation 
 {
     public List<String> letterCasePermutation(String s) {
-        
+        List<String>ans=new ArrayList<>();
+        solve(ans, 0, s.toCharArray());
+        return ans;
+    }
+
+    void solve(List<String>ans, int i, char ch[])
+    {
+        if(i==ch.length)
+        {
+            ans.add(new String(ch));
+            return;
+        }
+        if(Character.isLetter(ch[i]))
+        {
+            ch[i]=Character.toUpperCase(ch[i]);
+            solve(ans, i+1, ch);
+            ch[i]=Character.toLowerCase(ch[i]);
+            solve(ans, i+1, ch);
+        }
+        else
+        {
+            solve(ans, i+1, ch);
+        }
+    }
+
+    public static void main(String[] args) {
+        LetterCasePermutation letterCasePermutation=new LetterCasePermutation();
+        System.out.println(letterCasePermutation.letterCasePermutation("a1b2"));
     }
 }
