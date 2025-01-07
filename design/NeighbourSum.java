d455a2c13282497de5c779765ce0e439cc5d8901
shivam <shivamvish349@gmail.com>
2025-01-08 03:31:12 +0530

    added more problems

---
+++ design/NeighbourSum.java
@@ -0,0 +1,16 @@
+package design;
+
+// https://leetcode.com/problems/design-neighbor-sum-service/description/?envType=problem-list-v2&envId=mbnk3qwv
+public class NeighbourSum {
+    public NeighborSum(int[][] grid) {
+        
+    }
+    
+    public int adjacentSum(int value) {
+        
+    }
+    
+    public int diagonalSum(int value) {
+        
+    }
+}
---
+++ trees/PopulatingNextNodeII.java
@@ -0,0 +1,31 @@
+package trees;
+
+public class PopulatingNextNodeII 
+{
+    public Node connect(Node root) {
+        if(root==null)
+            return null;
+        if(root.left!=null)
+        {
+            if(root.right==null)
+                if(root.next!=null && root.next.left!=null)
+                    root.left.next=root.next.left;
+                else if(root.next!=null && root.next.right!=null)
+                    root.left.next=root.next.right;
+                else
+                    root.left.next=null;
+            else
+                root.left.next=root.right;
+        }
+        if(root.right!=null && root.next!=null)
+        {
+            if(root.next.left==null && root.next.right!=null)
+                root.right.next=root.next.right;
+            else if(root.next.left==null && root.next.right==null)
+                root.right.next=null;
+        }
+        connect(root.left);
+        connect(root.right);
+        return root;
+    } 
+}
