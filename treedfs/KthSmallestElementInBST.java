b97400c3666c5c13e2ba97a5b03edeb0a70a7fbc
shivam <shivamvish349@gmail.com>
2025-06-06 00:48:25 +0530

    Added more problems

--- treedfs/KthSmallestElementInBST.java
+++ treedfs/KthSmallestElementInBST.java
@@ -1,16 +1,29 @@
 package dailyproblems.treedfs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import dailyproblems.binarytree.TreeNode;
 
 public class KthSmallestElementInBST 
 {
     public int kthSmallest(TreeNode root, int k)
     {
-        
+        List<Integer>ans=new ArrayList<>();
+        solve(root, k, ans);
+        return ans.get(k-1);
     }
     
-    private void solve(TreeNode node)
+    private void solve(TreeNode node, int k, List<Integer>ans)
     {
-
+        if(node==null)
+            return;
+        if(node.left!=null)
+            solve(node.left, k, ans);
+        ans.add(node.val);
+        if(ans.size()==k)
+            return;
+        if(node.right!=null)
+            solve(node.right, k, ans);
     }
 }
